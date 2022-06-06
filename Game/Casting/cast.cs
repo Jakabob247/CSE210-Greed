d847c21455a29a670a20681a644fbbe569ac1206
eagleye123 <49323274+eagleye123@users.noreply.github.com>
2022-06-06 14:55:32 -0600

    added basic basic casting framework

---
+++ Game/Casting/cast.cs
@@ -0,0 +1,10 @@
+namespace CSE210-Greed.Casting.Game{
+    public class cast{
+        public void cast(){
+            Console.WriteLine("Hello World from the cast function")
+        }
+        public void AddActors(){
+            
+        }
+    }
+}
